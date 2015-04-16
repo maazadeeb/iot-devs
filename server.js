148ebbc4e8fc6c134dde56df69573f85b29e7935
msrit-gitlearn <kartik.gayatri@yahoo.in>
2015-04-16 17:59:12 +0530

    Testing rest

--- server.js
+++ server.js
@@ -104,6 +104,13 @@ var SampleApp = function() {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
         };
+        self.routes['/rest-test'] = function(req, res) {
+            res.setHeader('Content-Type', 'application/json');
+            var test_obj = {
+                "success":true
+            }
+            res.send(test_obj);
+        };
     };
 
 
