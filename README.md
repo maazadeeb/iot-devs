1f9d39703fb48c5d7cfd775b9f942bf6476bf132
maaz93 <maaz.adeeb@gmail.com>
2015-04-24 10:47:30 +0530

    Documentation and new end points

--- README.md
+++ README.md
@@ -1,17 +1,40 @@
-Repository for an IoT based project that aims at monitoring autistic students in real-time.
-
-## Setup local environment
+## Setting up local environment
 
 1. Install [Node.js](https://nodejs.org/download/)
 2. Install [MongoDB](https://www.mongodb.org/downloads). Try and put **`/{path of mongodb installation}/bin`** into your PATH of environment variables, so it becomes easy to run mongo db from the command line.
-3. Install [Git](http://git-scm.com/downloads). Make sure to select the "Use git from windows option".
+3. Install [Git](http://git-scm.com/downloads).
 4. Pull code to your local system: `git clone https://github.com/maaz93/iot-devs.git`
 5. `cd iot-devs`
 6. `npm install`
-7. Create a folder for MongoDB. Say `{path to your cloned repo}/db/data`. Open cmd and run `mongod --dbpath={folder structure}/iot-devs/db/data`. If you get any 'not recognised' error, then you havn't added mongod in environment variables list. Then run `/{path of mongodb installation}/bin/mongod --dbpath={folder structure}/iot-devs/db/data`. Minimize the cmd, **DO NOT CLOSE THE CMD**.
+7. Create a folder for MongoDB. Say `{folder structure}/iot-devs/db/data`. Open cmd and run `mongod --dbpath={folder structure}/iot-devs/db/data`. If you get any 'not recognised' error, then you havn't added mongod in environment variables list. Then run `/{path of mongodb installation}/bin/mongod --dbpath={folder structure}/iot-devs/db/data`. Minimize the cmd. **DO NOT CLOSE THE CMD** as the mongo server will be running here.
 8. `node /db/db.js` - This will create a default DB for you to use.
-9. `node server.js` - This will create the REST API. Test using [localhost:8080/listAll](http://localhost:8080/listAll). It will list the full DB in the browser.
+9. `node server.js` - This will create the REST API. Test using [localhost:8080/listAll](localhost:8080/listAll). It will list the full DB in the browser.
 
 ## Rest API documentation
 
-TODO.
+##### URLs for testing: [OpenShift](http://iot-sniper6.rhcloud.com/),[localhost](localhost:8080/)
+
+1. **`GET` `/listAll`**  
+* **Request Parameters**: None
+* **Reponse**: Details of all the students
+
+2. **`GET` `/list/{id}`**  
+* **Request Parameters**: {id} is the Shimmer ID
+* **Reponse**: Details of the student with Shimmer ID as {id}
+
+3. **`GET` `/sensor/{id}/{sensorName}`**  
+* **Request Parameters**: {id } is Shimmer ID, {sensorName} is the name of the sensor you need
+* **Reponse**: Timestamp and {sensorName} data of student
+* **Sample Request**: `/sensor/A643/gsr`
+* **Sample Response**: `{"timeStamp":[1,2,3,1,2,3],"gsr":[23,24,5,23,24,5]}`
+* Note: To access low range accelorometer use `/sensor/A643/accelerometer.lowRange`, i.e the dot notation to go inside objects.
+
+4. **`POST` `/student/{id}`**  
+* **Request Parameters**: {id} is the Shimmer ID, body **should** contain a **`data`** paramater as described below 
+* **Reponse**: JSON Object telling success or failure
+* **Sample Request**: `/student/A643`
+* **Sample Body**:
+```javascript
+{"timeStamp":[1,2,3],"accelerometer":{"lowRange":{"x":[3,2,4],"y":[7,4,3],"z":[1,5,3]},"highRange":{"x":[4,2,4],"y":[8,7,6],"z":[3,5,1]}},"gsr":[23,24,5],"temperature":[32,5,2],"pressure":[4,24,54],"gyroscope":[23,435,34],"magnetometer":[3,4,2],"adc13":[324,32,2]}
+```
+* **VERY IMPORTANT NOTE: The data HAS to be in the same format as the sample body. Key names CANNOT differ even by a letter** 
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 #!/bin/env node
-//  OpenShift sample Node application
+ //  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
+var fs = require('fs');
 var bodyParser = require('body-parser');
 var mongojs = require('mongojs');
+
 // var dbCon = require('./db-con/db.js');
 
 
@@ -15,6 +16,14 @@ var SampleApp = function() {
     //  Scope.
     var self = this;
 
+    // Static messages
+    var successJSON = {
+        "success": true
+    };
+
+    var failureJSON = {
+        "success": false
+    }
 
     /*  ================================================================  */
     /*  Helper functions.                                                 */
@@ -26,8 +35,8 @@ var SampleApp = function() {
     self.setupVariables = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-        
+        self.port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
@@ -35,17 +44,14 @@ var SampleApp = function() {
             self.ipaddress = "127.0.0.1";
         };
 
-        
-
         // default to a 'localhost' configuration:
         // self.connection_string = '127.0.0.1:27017/iot';
         // if OPENSHIFT env variables are present, use the available connection info:
-        if(process.env.OPENSHIFT_MONGODB_DB_URL) {
+        if (process.env.OPENSHIFT_MONGODB_DB_URL) {
 
             self.connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + "iot";
-            
-        }
-        else {
+
+        } else {
             console.warn('No OPENSHIFT_MONGODB_DB_URL var, using 127.0.0.1:27017');
             self.connection_string = '127.0.0.1:27017/iot';
         }
@@ -57,7 +63,9 @@ var SampleApp = function() {
      */
     self.populateCache = function() {
         if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
+            self.zcache = {
+                'index.html': ''
+            };
         }
 
         //  Local cache for static content.
@@ -69,7 +77,9 @@ var SampleApp = function() {
      *  Retrieve entry (content) from cache.
      *  @param {string} key  Key identifying content to retrieve from cache.
      */
-    self.cache_get = function(key) { return self.zcache[key]; };
+    self.cache_get = function(key) {
+        return self.zcache[key];
+    };
 
 
     /**
@@ -77,29 +87,33 @@ var SampleApp = function() {
      *  Terminate server on receipt of the specified signal.
      *  @param {string} sig  Signal to terminate on.
      */
-    self.terminator = function(sig){
+    self.terminator = function(sig) {
         if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           self.db.close();
-           process.exit(1);
+            console.log('%s: Received %s - terminating sample app ...',
+                Date(Date.now()), sig);
+            self.db.close();
+            process.exit(1);
         }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
+        console.log('%s: Node server stopped.', Date(Date.now()));
     };
 
 
     /**
      *  Setup termination handlers (for exit and a list of signals).
      */
-    self.setupTerminationHandlers = function(){
+    self.setupTerminationHandlers = function() {
         //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
+        process.on('exit', function() {
+            self.terminator();
+        });
 
         // Removed 'SIGPIPE' from the list - bugz 852598.
         ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+            'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
         ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
+            process.on(element, function() {
+                self.terminator(element);
+            });
         });
     };
 
@@ -112,32 +126,124 @@ var SampleApp = function() {
      *  Create the routing table entries + handlers for the application.
      */
     self.createRoutes = function() {
-        self.getRoutes = { };
-        self.postRoutes = { };
+        self.getRoutes = {};
+        self.postRoutes = {};
 
+        // default page for landing. HAProxy error avoidance
         self.getRoutes['/'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
+            res.send(self.cache_get('index.html'));
         };
 
+        // list all the students in the DB
         self.getRoutes['/listAll'] = function(req, res) {
             console.log("Inside listAll...");
             res.setHeader('Content-Type', 'application/json');
-            self.db.students.find( function(err, docs) {
-                if(err) {
-                    res.send(500);
-                }
-                else {
+            self.db.students.find(function(err, docs) {
+                if (err) {
+                    res.json(failureJSON);
+                } else {
                     res.json(docs);
                 }
             });
         };
 
-        self.postRoutes['/student'] = function(req, res) {
-            // var id = req.body.id;
+        // list out the particular Shimmer ID's details
+        self.getRoutes['/list/:id'] = function(req, res) {
+            console.log("Inside list %s...", req.params.id);
+            var shimmerId = req.params.id;
+            res.setHeader('Content-Type', 'application/json');
+            self.db.students.findOne({
+                    shimmerId: shimmerId
+                },
+                function(err, doc) {
+                    if (err) {
+                        res.json(failureJSON);
+                    } else {
+                        res.json(doc)
+                    }
+                });
+        };
+
+        // send out data for the graphs for Shimmer ID
+        self.getRoutes['/sensor/:id/:name'] = function(req, res) {
+            var shimmerId = req.params.id;
+            var sensor = req.params.name;
+            var filter = {};
+            filter[sensor] = 1;
+            filter["timeStamp"] = 1;
+            filter["_id"] = 0;
+            console.log("Inside sensor %s for %s", sensor, shimmerId);
+            self.db.students.findOne({
+                    shimmerId: shimmerId
+                },
+                filter,
+                function(err, doc) {
+                    if (err) {
+                        res.json(failureJSON);
+                    } else {
+                        res.json(doc);
+                    }
+                });
+
+        };
+
+        // accept data to particular Shimmer ID
+        // TODO: Bad way of pushing data. Make it more modular
+        self.postRoutes['/student/:id'] = function(req, res) {
             console.log("Inside student...");
             res.setHeader('Content-Type', 'application/json');
-            res.send(req.body);
+            var shimmerId = req.params.id;
+            var sensorData = JSON.parse(req.body.data);
+            self.db.students.update({
+                    shimmerId: shimmerId
+                }, {
+                    $push: {
+                        timeStamp: {
+                            $each: sensorData.timeStamp
+                        },
+                        'accelerometer.lowRange.x': {
+                            $each: sensorData.accelerometer.lowRange.x
+                        },
+                        'accelerometer.lowRange.y': {
+                            $each: sensorData.accelerometer.lowRange.y
+                        },
+                        'accelerometer.lowRange.z': {
+                            $each: sensorData.accelerometer.lowRange.z
+                        },
+                        'accelerometer.highRange.x': {
+                            $each: sensorData.accelerometer.highRange.x
+                        },
+                        'accelerometer.highRange.y': {
+                            $each: sensorData.accelerometer.highRange.y
+                        },
+                        'accelerometer.highRange.z': {
+                            $each: sensorData.accelerometer.highRange.z
+                        },
+                        gsr: {
+                            $each: sensorData.gsr
+                        },
+                        temperature: {
+                            $each: sensorData.temperature
+                        },
+                        pressure: {
+                            $each: sensorData.pressure
+                        },
+                        gyroscope: {
+                            $each: sensorData.gyroscope
+                        },
+                        magnetometer: {
+                            $each: sensorData.magnetometer
+                        },
+                        adc13: {
+                            $each: sensorData.adc13
+                        }
+                    }
+                },
+                function() {
+                    res.json(successJSON);
+                }
+            );
         };
     };
 
@@ -149,7 +255,9 @@ var SampleApp = function() {
     self.initializeServer = function() {
         self.createRoutes();
         self.app = express();
-        self.app.use(bodyParser.urlencoded({extended:true}));
+        self.app.use(bodyParser.urlencoded({
+            extended: true
+        }));
         self.app.use(bodyParser.json());
         self.app.use(bodyParser.raw());
         self.db = mongojs(self.connection_string, ['students']);
@@ -188,12 +296,12 @@ var SampleApp = function() {
         //  Start the app on the specific interface (and port).
         self.app.listen(self.port, self.ipaddress, function() {
             console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+                Date(Date.now()), self.ipaddress, self.port);
             console.log('Mongo DB running on: %s', self.connection_string);
         });
     };
 
-};   /*  Sample Application.  */
+}; /*  Sample Application.  */
 
 
 
